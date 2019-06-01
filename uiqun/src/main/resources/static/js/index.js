4e3b934f1c838b26fa97786fcf99bd989ce33741
david751105 <elecland@126.com>
2019-06-01 14:02:05 +0800

    修改首页Banner

--- uiqun/src/main/resources/static/js/index.js
+++ uiqun/src/main/resources/static/js/index.js
@@ -1,15 +1,9 @@
-/**
- * Created by David on 2019-01-08.
- */
 
-
-$(document).ready(function(){
-    $("li:even").css({"background-color":"#F0F0F0"});
-});
 
 $(function(){
     $("#content>ul>span").html("")
 })
+
 function inquote(rfqno) {
     window.location.href=getRootPath()+"/inquote/"+rfqno;
 }
