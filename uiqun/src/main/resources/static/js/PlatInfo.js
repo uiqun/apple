54d219aa8611c4ceadf49ac153d0be17c73a63b2
david751105 <elecland@126.com>
2019-06-17 11:56:19 +0800

    优化了最顶端导航

--- uiqun/src/main/resources/static/js/PlatInfo.js
+++ uiqun/src/main/resources/static/js/PlatInfo.js
@@ -6,31 +6,31 @@ $(function(){
     $("ul li:first-child").click(function(){
         $(this).css("color","#FF0000");
         $(this).siblings().css("color","black");
-        $("p:first-child").show();
-        $("p:first-child").siblings().hide();
+        $("textarea:first-child").show();
+        $("textarea:first-child").siblings().hide();
     })
     $("ul li:nth-child(2)").click(function(){
         $(this).css("color","#FF0000");
         $(this).siblings().css("color","black");
-        $("p:nth-child(2)").show();
-        $("p:nth-child(2)").siblings().hide();
+        $("textarea:nth-child(2)").show();
+        $("textarea:nth-child(2)").siblings().hide();
     })
     $("ul li:nth-child(3)").click(function(){
         $(this).css("color","#FF0000");
         $(this).siblings().css("color","black");
-        $("p:nth-child(3)").show();
-        $("p:nth-child(3)").siblings().hide();
+        $("textarea:nth-child(3)").show();
+        $("textarea:nth-child(3)").siblings().hide();
     })
     $("ul li:nth-child(4)").click(function(){
         $(this).css("color","#FF0000");
         $(this).siblings().css("color","black");
-        $("p:nth-child(4)").show();
-        $("p:nth-child(4)").siblings().hide();
+        $("textarea:nth-child(4)").show();
+        $("textarea:nth-child(4)").siblings().hide();
     })
     $("ul li:nth-child(5)").click(function(){
         $(this).css("color","#FF0000");
         $(this).siblings().css("color","black");
-        $("p:nth-child(5)").show();
-        $("p:nth-child(5)").siblings().hide();
+        $("textarea:nth-child(5)").show();
+        $("textarea:nth-child(5)").siblings().hide();
     })
 })
