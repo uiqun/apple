71a3bb3d5317dff32c056451d30d0f8abfaa980b
OverYearLikeWater <504317962@qq.com>
2019-06-18 22:23:40 +0800

    更新了  权限管理

--- uiqun/src/main/resources/static/js/vendor.js
+++ uiqun/src/main/resources/static/js/vendor.js
@@ -10,37 +10,28 @@ $(function(){
         $(this).siblings().find("ol").hide();
     })
 })
-// var checkCurrentUserInfoVar =null;
+var checkCurrentUserInfoVar =null;
 function checkCurrentUserInfo(obj) {
     $(obj)[0].onmousemove=function (evt) {
         var oEvent = evt || window.event;
         var scrollleft = document.documentElement.scrollLeft || document.body.scrollLeft;
         var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
-        // if(checkCurrentUserInfoVar==null||checkCurrentUserInfoVar.uid!=$(obj).attr("value")) {
+        if(checkCurrentUserInfoVar==null||checkCurrentUserInfoVar.uid!=$(obj).attr("value")) {
             $.post("/user/queryUserInfo", {"userId": $(obj).attr("value")}, function (result) {
                 if (result != null) {
-                    // checkCurrentUserInfoVar=result;
-                    $("#currentUserInfo thead th").html(result.company);
-                    $("#currentUserInfo tbody>tr:first-child>td:nth-of-type(2)").html(result.contact==null?"":result.contact);
-                    $("#currentUserInfo tbody>tr:first-child>td:nth-of-type(4)").html(result.tel==null?"":result.tel);
-                    $("#currentUserInfo tbody>tr:nth-of-type(2)>td:nth-of-type(2)").html(result.qq==null?"":result.qq);
-                    $("#currentUserInfo tbody>tr:nth-of-type(2)>td:nth-of-type(4)").html(result.wechat==null?"":result.wechat);
-                    $("#currentUserInfo tfoot td:nth-of-type(2)").html(result.addr);
-                    document.getElementById("currentUserInfo").style.display = "block";
-                    document.getElementById("currentUserInfo").style.left = oEvent.clientX + scrollleft + 10 + "px";
-                    document.getElementById("currentUserInfo").style.top = oEvent.clientY + scrolltop + 10 + "px";
+                    checkCurrentUserInfoVar=result;
                 }
             }, "JSON")
-        // }
-        // $("#currentUserInfo thead th").html(checkCurrentUserInfoVar.company);
-        // $("#currentUserInfo tbody>tr:first-child>td:nth-of-type(2)").html(checkCurrentUserInfoVar.contact);
-        // $("#currentUserInfo tbody>tr:first-child>td:nth-of-type(4)").html(checkCurrentUserInfoVar.tel);
-        // $("#currentUserInfo tbody>tr:nth-of-type(2)>td:nth-of-type(2)").html(checkCurrentUserInfoVar.qq);
-        // $("#currentUserInfo tbody>tr:nth-of-type(2)>td:nth-of-type(4)").html(checkCurrentUserInfoVar.wechat);
-        // $("#currentUserInfo tfoot td:nth-of-type(2)").html(checkCurrentUserInfoVar.addr);
-        // document.getElementById("currentUserInfo").style.display = "block";
-        // document.getElementById("currentUserInfo").style.left = oEvent.clientX + scrollleft + 10 + "px";
-        // document.getElementById("currentUserInfo").style.top = oEvent.clientY + scrolltop + 10 + "px";
+        }
+        $("#currentUserInfo thead th").html(checkCurrentUserInfoVar.company==null?"":checkCurrentUserInfoVar.company);
+        $("#currentUserInfo tbody>tr:first-child>td:nth-of-type(2)").html(checkCurrentUserInfoVar.contact==null?"":checkCurrentUserInfoVar.contact);
+        $("#currentUserInfo tbody>tr:first-child>td:nth-of-type(4)").html(checkCurrentUserInfoVar.tel==null?"":checkCurrentUserInfoVar.tel);
+        $("#currentUserInfo tbody>tr:nth-of-type(2)>td:nth-of-type(2)").html(checkCurrentUserInfoVar.qq==null?"":checkCurrentUserInfoVar.qq);
+        $("#currentUserInfo tbody>tr:nth-of-type(2)>td:nth-of-type(4)").html(checkCurrentUserInfoVar.wechat==null?"":checkCurrentUserInfoVar.wechat);
+        $("#currentUserInfo tfoot td:nth-of-type(2)").html(checkCurrentUserInfoVar.addr==null?"":checkCurrentUserInfoVar.addr);
+        document.getElementById("currentUserInfo").style.display = "block";
+        document.getElementById("currentUserInfo").style.left = oEvent.clientX + scrollleft + 10 + "px";
+        document.getElementById("currentUserInfo").style.top = oEvent.clientY + scrolltop + 10 + "px";
     }
 }
 
