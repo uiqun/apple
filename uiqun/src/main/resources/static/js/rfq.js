34e25d75dd6c4b2aa295cd26119920f54d828d37
david751105 <elecland@126.com>
2019-06-19 10:34:56 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	uiqun/src/main/resources/templates/BOM.html
    #	uiqun/src/main/resources/templates/addMfg.html
    #	uiqun/src/main/resources/templates/addPn.html
    #	uiqun/src/main/resources/templates/company.html
    #	uiqun/src/main/resources/templates/findPrice.html
    #	uiqun/src/main/resources/templates/hotStk.html
    #	uiqun/src/main/resources/templates/index.html
    #	uiqun/src/main/resources/templates/login.html
    #	uiqun/src/main/resources/templates/platInfo.html
    #	uiqun/src/main/resources/templates/quote.html
    #	uiqun/src/main/resources/templates/regist.html
    #	uiqun/src/main/resources/templates/rfq.html
    #	uiqun/src/main/resources/templates/rfqDetail.html
    #	uiqun/src/main/resources/templates/userInfo.html
    #	uiqun/src/main/resources/templates/vendor.html

--- uiqun/src/main/resources/static/js/rfq.js
+++ uiqun/src/main/resources/static/js/rfq.js
@@ -4,30 +4,37 @@
 
 $(document).ready(function(){
     $("li:odd").css("background","#F0F0F0")
-
 })
 function checkRfqPn(){
      var pn = document.getElementsByName("pn")[0].value;
     if(pn==null||pn==''){
-        document.getElementById("mfg").innerHTML="型号不能为空";
+        document.getElementById("alert").innerHTML="型号不能为空";
         return;
+    }else {
+        document.getElementById("alert").innerHTML="";
     }
     $.post("/checkRfqPn",{"pn":pn},function (date){
         if(date.state=="none"){
-            document.getElementById("mfg").innerHTML=date.message;
+            document.getElementById("alert").innerHTML=date.message;
+            document.getElementById("create").style.display="block";
+            document.getElementById("mfg").innerHTML="品牌";
         }else{
             if(date.datas.length>0) {
                 var tdSelect = document.getElementById("mfg");
                 tdSelect.innerHTML = "";
                 var select = document.createElement("select");
                 select.setAttribute("name", "mfg");
+                select.style.fontSize="18px";
                 tdSelect.append(select);
                 for (i = 0; i < date.datas.length; i++) {
                     var option = document.createElement("option");
                     option.setAttribute("value", date.datas[i].mfgName);
                     option.innerText = date.datas[i].mfgName;
+                    option.style.fontSize="18px";
                     tdSelect.children[0].append(option);
                 }
+                document.getElementById("alert").innerHTML="";
+                document.getElementById("create").style.display="none";
             }
         }
 
